f3731431710f9652b95d175abd32197d81af5aa3
AminaAhllal <=>
2024-08-05 09:51:13 +0100

    tailwind to css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,38 @@
-import React from 'react'
-import Logo from './Logo'
+import React from 'react';
+import Logo from './Logo';
 import { GrSearch } from "react-icons/gr";
 import { FaRegUserCircle } from "react-icons/fa";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import './Header.css';
 
 const Header = () => {
   return (
-    <header className='h-16 bg-white'>
-      <div className=' h-full container mx-auto flex items-center px-4  justify-between'>
-        <div className=''>
+    <header className='header'>
+      <div className='header-container'>
+        <div className='logo-link'>
           <Link to={"/"}>
-          <Logo w={100} h={60}/>
+            <Logo w={100} h={60} />
           </Link>
         </div>
-        <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
-                <input type='text' placeholder='search here...' className='w-full outline-none'/>
-                <div className='text-lg min-w-[50px] h-8 bg-[#1F6F78] flex items-center justify-center rounded-r-full text-white'>
-                  <GrSearch />
-                </div>
-            </div>
-
-      <div className='flex items-center gap-7'>
-        <div>
-          <Link to = {"/login"} className='text-3xl cursor-pointer'>
-          <FaRegUserCircle/>
-          </Link>
+        <div className='search-bar-lg'>
+          <input type='text' placeholder='search here...' className='search-input' />
+          <div className='search-button'>
+            <GrSearch />
+          </div>
         </div>
-        
-        
-
-        <div>
-          <Link  to ={"/" }className='px-3 py-1 text-xl font-bold hover:underline'>Logout</Link>
+        <div className='user-actions'>
+          <div>
+            <Link to={"/login"} className='user-link'>
+              <FaRegUserCircle />
+            </Link>
+          </div>
+          <div>
+            <Link to={"/"} className='logout-link'>Logout</Link>
+          </div>
         </div>
-       </div>
-
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,87 +1,85 @@
-import React, { useState } from 'react'
-import Logo from '../assest/Logo.svg'
-import { FaEye } from "react-icons/fa";
-import { FaEyeSlash } from "react-icons/fa";
+import React, { useState } from 'react';
+import Logo from '../assest/Logo.svg';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import './Login.css';
 
-const Login =() => {
-    const [showPassword,setShowPasword] = useState(false)  
-    const[data,setData] = useState({
-        email : "",
-        password : ""
-    })
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
-    const handleOnChange = (e) =>{
-        const{name,value} = e.target
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
 
-        setData((preve)=>{
-            return{
-                ...preve,
-                [name] : value
-            }
-        })
-    }
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-    }
-    console.log("data login",data)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  console.log('data login', data);
   return (
     <section id='login'>
-        <div className='mx-auto container p-4'>
-            
-            <div className='bg-white  border  border-white p-5 w-full max-w-sm mx-auto'>
-                <div className='w-20 h-20 mx-auto '>
-                    <img src={Logo} alt='Login icons'/>
-                </div>
-                <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
-                    <div>
-                        <label>Email : </label>
-                        <div  className='bg-slate-100 border border-[#cfc6c6] rounded-full p-2 '>
-                                <input 
-                                type='text' 
-                                placeholder='Enter email'  
-                                name='email' 
-                                value= {data.email} 
-                                onChange={handleOnChange} 
-                                className='w-full h-full outline-none bg-transparent'/> 
-                        </div>
-                    </div>
-                    <div>
-                        <label>Password :</label>
-                        <div  className='bg-slate-100 border border-[#cfc6c6] rounded-full p-2 flex '>
-                                <input 
-                                type={showPassword ? "text" : "password"}   
-                                placeholder='Enter password' 
-                                value={data.password} 
-                                name='password'
-                                onChange={handleOnChange}   
-                                className='w-full h-full outline-none bg-transparent border-black'/>
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowPasword((preve)=>!preve)}>
-                            <span>
-                                {
-                                    showPassword ? (
-                                        <FaEyeSlash/>
-                                    )
-                                    :
-                                    (
-                                        <FaEye/>
-                                    )
-                                }
-                            </span>
-                        </div>
-                    </div>
-                    <Link to={'/forget-password'} className='block w-fit ml-auto hover:underline hover:text-[#1F6F78]'>
-                        Forget password?
-                    </Link>
-                    </div>
-                   <button className='bg-[#1F6F78]  hover:bg-[#14474d]  text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-5'>Login</button>
-                </form>
-                <p className="my-5"> Don't have account?<Link to={'/sign-up'} className=' text-[#1F6F78]  hover:text-[#4a6264] hover:underline'> Sign up</Link></p>
+      <div className='container'>
+        <div className='login-box'>
+          <div className='logo'>
+            <img src={Logo} alt='Login icon' />
+          </div>
+          <form className='form' onSubmit={handleSubmit}>
+            <div>
+              <label>Email :</label>
+              <div className='input-group'>
+                <input
+                  type='text'
+                  placeholder='Enter email'
+                  name='email'
+                  value={data.email}
+                  onChange={handleOnChange}
+                  className='input'
+                />
+              </div>
             </div>
+            <div>
+              <label>Password :</label>
+              <div className='input-group password-group'>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Enter password'
+                  value={data.password}
+                  name='password'
+                  onChange={handleOnChange}
+                  className='input'
+                />
+                <div
+                  className='password-toggle'
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+              </div>
+            </div>
+            <button className='login-button'>Login</button>
+          </form>
+          <p className='signup-text'>
+            Don't have an account?
+            <Link to='/sign-up' className='signup-link'>
+              {' '}
+              Sign up
+            </Link>
+          </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
